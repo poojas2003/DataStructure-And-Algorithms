5f0ea1804b7159de9ab8c6d70ba8d265bdea8f00
poojas2003 <96231234+poojas2003@users.noreply.github.com>
2026-03-11 11:10:48 +0530

    SlidingWindow.py

---
+++ Array/SlidingWindow/slidingWindow.py
@@ -0,0 +1,17 @@
+A=[1,2,3,4,5,6,-1]
+K=4
+n=len(A)
+start=0
+end=K-1
+
+result=float('-inf')
+
+while(e<n):
+    subArraySum=0
+    for i in range(start,end+1):
+        subArraySum+=A[i]
+    result=max(subArraySum,result)
+    start+=1
+    end+=1
+        
+print(result);
