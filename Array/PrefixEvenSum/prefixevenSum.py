4a7a66b308bbaa5c387952ca17547eb9e4f01138
poojas2003 <96231234+poojas2003@users.noreply.github.com>
2026-03-26 19:17:07 +0530

    Add even prefix sum functionality
    Implement even prefix sum calculation for an array.

--- Array/PrefixEvenSum/prefixevenSum.py
+++ Array/PrefixEvenSum/prefixevenSum.py
@@ -1,1 +1,12 @@
+#even prefix sum
 
+A=[1,2,3,4,5]
+even_prefix=[0]*len(A)
+even_prefix[0]=A[0]
+for i in range(1,len(A)):
+    if i%2==0:
+        even_prefix[i]=even_prefix[i-1]+A[i]
+    else:
+        even_prefix[i]=even_prefix[i-1]
+        
+print(even_prefix) 
