60f4e34bd7916fffa10056a754cead376e2c6360
poojas2003 <96231234+poojas2003@users.noreply.github.com>
2026-03-27 16:26:46 +0530

    Update PostFixExpression.py

--- LinkedList/PostFixExpression/PostFixExpression.py
+++ LinkedList/PostFixExpression/PostFixExpression.py
@@ -1,1 +1,25 @@
+def postExpression(s):
+    stack = []
 
+    for ch in s:
+        print("Processing:", ch)
+
+        if ch.isdigit():
+            stack.append(int(ch))
+        else:
+            a = stack.pop()
+            b = stack.pop()
+
+            if ch == '+':
+                stack.append(b + a)
+            elif ch == '-':
+                stack.append(b - a)
+            elif ch == '*':
+                stack.append(b * a)
+            elif ch == '/':
+                stack.append(int(b / a))
+        print("Stack:", stack)
+    return stack[0]
+
+
+print(postExpression("23*456+-5--"))   
